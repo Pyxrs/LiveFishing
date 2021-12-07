a566f553020afe32eaac283b3b17f97b239a58cd
Jab125 <67534807+Jab125@users.noreply.github.com>
2021-12-07 18:50:26 +1100

    Create FishData.java

---
+++ src/main/java/io/github/simplycmd/fishing/data/FishData.java
@@ -0,0 +1,8 @@
+package io.github.simplycmd.fishing.data;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.item.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+public record FishData(@NotNull EntityType<?> fish, ItemStack itemStack) {
+}
